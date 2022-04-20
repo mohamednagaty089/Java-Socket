2195a77b3464b3e42430969d6e3ef50bb6ee9245
mohamed <mohamed.yhaoo.com>
2022-04-20 11:24:22 +0200

    server can accept three client from  queue

--- src/Server.java
+++ src/Server.java
@@ -1,37 +1,56 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try(ServerSocket server=new ServerSocket(9090);
-				Socket client=server.accept();
-				InputStream clientsendtoserver=client.getInputStream();
-				OutputStream serversendtoclient=client.getOutputStream();
-				Reader clientinput=new InputStreamReader(clientsendtoserver);
-				BufferedReader clientdata=new BufferedReader(clientinput);
-				PrintStream serverdata=new PrintStream(serversendtoclient);){
-			    
-			String message=clientdata.readLine();
-			serverdata.println("well come "+message);
-			String line;
-			int count=0;
-			while(count<=5) {
-				count++;
-				line=clientdata.readLine();
-				System.out.println(line);
+		try (ServerSocket server = new ServerSocket(9999);) {
+			int clientcounter = 0;
+			while (clientcounter <= 2) {
+				clientcounter++;
+
+				try (// defination server
+						Socket client = server.accept(); // open connection between server and client
+						InputStream clientsendtoserver = client.getInputStream();
+						OutputStream serversendtoclient = client.getOutputStream();
+						Reader clientinput = new InputStreamReader(clientsendtoserver);
+						BufferedReader clientdata = new BufferedReader(clientinput);
+						PrintStream serverdata = new PrintStream(serversendtoclient);) {
+					ArrayList<String> inputtofile = new ArrayList<String>();
+					String message = clientdata.readLine();
+					inputtofile.add(message);
+					serverdata.println("well come " + message);
+					inputtofile.add("well come " + message);
+					String line;
+					int count = 0;
+					while (count <= 5) {
+						count++;
+						line = clientdata.readLine();
+						inputtofile.add(line);
+						System.out.println(line);
+					}
+					File f = new File("C:\\Users\\Samy\\Desktop\\java\\naga.txt");
+					PrintWriter insertdata = new PrintWriter(f);
+					for (String line1 : inputtofile) {
+						insertdata.println(line1);
+					}
+					insertdata.close();
+
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
 			}
-			
-			    
-			
-		}catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
